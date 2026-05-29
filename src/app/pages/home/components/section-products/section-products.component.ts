8848412deb2b811635116ca76e3072ab15149324
Your Name <julianvelandia3110@gmail.com>
2026-05-29 15:16:47 -0500

    fix : ajuste seccion productos

--- src/app/pages/home/components/section-products/section-products.component.ts
+++ src/app/pages/home/components/section-products/section-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsService } from '../../../../core/services/products.service';
 import { ProductWithImages } from '../../../../core/models/product.model';
@@ -13,6 +13,7 @@ import { environment } from '../../../../../environments/environment';
 })
 export class SectionProductsComponent implements OnInit {
   private productsService = inject(ProductsService);
+  private cdr = inject(ChangeDetectorRef);
 
   products: ProductWithImages[] = [];
   loading = true;
@@ -23,10 +24,12 @@ export class SectionProductsComponent implements OnInit {
       next: data => {
         this.products = data;
         this.loading = false;
+        this.cdr.detectChanges()
       },
       error: () => {
         this.error = true;
         this.loading = false;
+        this.cdr.detectChanges();
       }
     });
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api'
+  apiUrl: 'https://gerencia-backend.onrender.com/api'
 };
